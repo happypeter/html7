52eeb1ea6292a8f4dbb3fd4a81bc66c47a0acadc
Peter Wang <happypeter1983@gmail.com>
2013-12-01 20:22:44 +0800

    i

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $('.subnav ul li').on('click', function() {
-    $('.subnav ul li').removeClass('active');
-
+  $('.user-detail-nav ul li').on('click', function() {
+    $('.user-detail-nav ul li').removeClass('active');
     var myClass = $(this).attr("class");
     $(this).toggleClass('active');
-    $('.books').children().hide();
-    $('.books').children('.' + myClass).show();
+    $('.user-detail-content').children().hide();
+    $('.user-detail-content').children('.' + myClass).show();
+    return false;
 
   });
 
